dfe7458c99e3dbae3840a79e645776fd27518c7f
nirbhay patil <nirbhaypatil@gmail.com>
2023-12-02 10:44:35 +0530

    re-adding throws to function.

--- src/test/java/testcases/TestCases.java
+++ src/test/java/testcases/TestCases.java
@@ -1,24 +1,18 @@
 package testcases;
 
 
-import keywords.Keyword;
 import base.TestBase;
-import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 import pages.MainMenu;
 import pages.ProductItemDetailsPage;
 import pages.SearchResultsPage;
-import waits.WaitFor;
-
-import java.util.List;
 
 public class TestCases extends TestBase {
 
     @Test
     public void verifyNoKidsShortsInBag() throws InterruptedException {
-
+        
         MainMenu menu = new MainMenu();
         //go to kids -> shorts menu
         menu.hoverOn("kids");
@@ -34,4 +28,23 @@ public class TestCases extends TestBase {
         Assert.assertEquals(error,"Please select a size");
 
     }
+
+    @Test
+    public void verifySelectedProductGetsAddedInBag() throws InterruptedException {
+
+        MainMenu menu = new MainMenu();
+        //go to kids -> shorts menu
+        menu.hoverOn("MEN");
+        menu.clickOnMenuItem("Jeans");
+
+        SearchResultsPage searchResults = new SearchResultsPage();
+        searchResults.selectFirstProduct();
+        ProductItemDetailsPage productItem = new ProductItemDetailsPage();
+        //Choose size of product
+        productItem.selectDefaultSize();
+        productItem.addToBag();
+        //Choose size of product
+        Assert.assertEquals(productItem.isItemAddedInBag(),"1");
+    }
+
 }
